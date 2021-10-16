fbbb8b7dc2af70b4d4e028ce8e20294fbb8672ef
aymen <adirar111@gmail.com>
2021-10-15 17:51:22 -0700

    [frontend] prettier! (#4)

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>frontend start</h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>frontend start</h1>
+    </div>
+  );
 }
 
 export default App;
