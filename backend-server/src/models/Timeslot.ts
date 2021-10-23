83b7201fa3d2a01bc22cbf3a5310d19536747a44
Emily Chen <emilylynnchen@gmail.com>
2021-10-23 12:28:09 -0700

    minor changes to model

--- backend-server/src/models/Timeslot.ts
+++ backend-server/src/models/Timeslot.ts
@@ -2,12 +2,12 @@ export default class Timeslot {
     public readonly startTime: Date;
     public readonly duration: number;
     public readonly isBooked: boolean;
-    public readonly interviewee: string;
+    public readonly email: string;
 
-    constructor(startTime: Date, duration: number, isBooked: boolean, interviewee: string) {
+    constructor(startTime: Date, duration: number, isBooked: boolean, email: string) {
         this.startTime = startTime;
         this.duration = duration;
         this.isBooked = isBooked;
-        this.interviewee = interviewee;
+        this.email = email;
     }
 }
