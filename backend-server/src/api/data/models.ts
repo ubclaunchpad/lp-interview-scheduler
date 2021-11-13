86d45d28a9cb6b9f8149452a5077541ba86b5648
hhanray <hhanray@gmail.com>
2021-11-12 21:39:57 -0800

    added event request body models, updated events to use array of leads

--- backend-server/src/api/data/models.ts
+++ backend-server/src/api/data/models.ts
@@ -17,9 +17,7 @@ export interface Availability {
 }
 
 export interface Event {
-  organization: string;
-  lead1: string;
-  lead2: string;
+  leads: Array<{ leadUID: string; name: string }>;
   intervieweeEmail: string;
   confirmedTime: string;
   length: number;
