7323c4a240227aede8ce5077ddf2e99706f85841
Tricia-Faith-Cu <triciafaithcu@gmail.com>
2021-12-03 00:18:13 -0800

    changed BookingPage.tsx to CreateLinkPage.tsx, added corresponding router changes

--- frontend-react/src/components/AdminApp.tsx
+++ frontend-react/src/components/AdminApp.tsx
@@ -2,6 +2,7 @@ import AuthenticatedApp from "./AuthenticatedApp";
 import UnauthenticatedApp from "./UnauthenticatedApp";
 import { useAuth } from "../contexts/AuthContext";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import CreateLinkPage from "./CreateLinkPage";
 
 export default function AdminApp() {
   const { user } = useAuth();
@@ -14,6 +15,7 @@ export default function AdminApp() {
         <Route exact path="/app/authorized">
           {user ? <AuthenticatedApp /> : <Redirect to="/app/unauthorized" />}
         </Route>
+        <Route path="/app/authorized/createlink" component={CreateLinkPage} />
         <Redirect from="/app" to="/app/unauthorized" />
       </Switch>
     </BrowserRouter>
