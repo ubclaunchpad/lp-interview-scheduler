caff72c90df9d01852fe1338009039be46769a2d
Jin Kim <jink8036@gmail.com>
2022-02-17 20:35:13 -0800

    Remove TODOs and fix dev alerts

--- frontend-react/src/components/BookingPage.tsx
+++ frontend-react/src/components/BookingPage.tsx
@@ -4,16 +4,18 @@ import InterviewTimePicker from "./booking/InterviewTimePicker";
 import moment, { Moment } from "moment";
 
 interface APIAvailability {
-  durationMins: number,
-  startTime: string,
-  bookedByEmail: string,
-  interviewerUID: string,
-  isBooked: boolean
+  durationMins: number;
+  startTime: string;
+  bookedByEmail: string;
+  interviewerUID: string;
+  isBooked: boolean;
 }
 
 const linkPrefix = "http://localhost:8080/v1/";
 export default function PageThree() {
-  const [mergedAvailabilities, setAvailabilities] = useState([] as APIAvailability[]);
+  const [mergedAvailabilities, setAvailabilities] = useState(
+    [] as APIAvailability[]
+  );
   const [leadUIDs, setLeadUIDs] = useState([] as string[]);
   const [confirmedTime, setConfirmedTime] = useState("");
 
@@ -41,8 +43,6 @@ export default function PageThree() {
 
   // Call get request with lead IDs from event body and return merged availabilities
   async function handleMergeAvailabilities(eventBody: any) {
-    // TOOD : Request to get merged availabilities
-    // TODO : Set state so that we have the availability?
     try {
       const lead1_UID = eventBody["leads"][0]["leadUID"];
       const lead2_UID = eventBody["leads"][1]["leadUID"];
@@ -53,59 +53,64 @@ export default function PageThree() {
           `availabilities/mergedTimes/?organization=${organization}&interviewerUID1=${lead1_UID}&interviewerUID2=${lead2_UID}`
       );
       if (!response.ok) {
-        alert("Get Event request failed");
+        alert("Merged availability request failed");
       }
       const data = await response.json();
       setAvailabilities(data);
       console.log(data);
     } catch (e) {
       console.log(e);
-      alert("Something went wrong with get Event request");
+      alert("Something went wrong with merged availability request");
     }
   }
 
   const apiAvailsToMoments = (avails: APIAvailability[]) => {
     return avails.map((a) => moment(a.startTime));
-  }
+  };
 
   React.useEffect(() => {
     if (params.has("eventUID") && params.has("organization")) {
       handleGetEvent().then((data) => handleMergeAvailabilities(data));
     }
-    
   }, []);
-
 
   const bookSlot = (slot: Moment) => {
     fetch(linkPrefix + "events", {
-        method: 'PATCH',
-        headers: { 'Content-Type':'application/json' },
-        body: JSON.stringify({
-          organization: organization,
-          eventUID: eventUID,
-          leadUIDs: leadUIDs,
-          times: [slot.format("YYYY-MM-DDTHH:mm:ssZ")]
-        })
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        organization: organization,
+        eventUID: eventUID,
+        leadUIDs: leadUIDs,
+        times: [slot.format("YYYY-MM-DDTHH:mm:ssZ")],
+      }),
     }).then((res) => {
       if (res.status == 200) {
         setConfirmedTime(slot.toString());
-        window.alert("You have successfully booked an interview for " + slot.format('LLLL'));
+        window.alert(
+          "You have successfully booked an interview for " + slot.format("LLLL")
+        );
       } else {
         window.alert("Error booking interview.");
       }
     });
-  }
+  };
 
   if (confirmedTime != null && confirmedTime.length > 0) {
     return (
       <div>
-        <p>Your interview is booked for {moment(confirmedTime).format("LLLL")}</p>
+        <p>
+          Your interview is booked for {moment(confirmedTime).format("LLLL")}
+        </p>
       </div>
-    )
+    );
   } else {
     return (
       <div>
-        <InterviewTimePicker availabilities={apiAvailsToMoments(mergedAvailabilities)} onBook={bookSlot}></InterviewTimePicker>
+        <InterviewTimePicker
+          availabilities={apiAvailsToMoments(mergedAvailabilities)}
+          onBook={bookSlot}
+        ></InterviewTimePicker>
       </div>
     );
   }
