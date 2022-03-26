97d3811ee9c77685e3acb04ba79af8a1c0460566
Aymen Dirar <63937200+adirar111@users.noreply.github.com>
2022-03-26 15:20:28 -0700

    page 3 styling

--- frontend-react/src/components/booking/Day.tsx
+++ frontend-react/src/components/booking/Day.tsx
@@ -1,29 +1,32 @@
-import styles from './Day.module.css';
+import styles from "./Day.module.css";
 
 interface Props {
-    date: Number,
-    onClick: Function,
-    highlight: boolean,
-    selected: boolean,
-    disabled: boolean
+  date: Number;
+  onClick: Function;
+  highlight: boolean;
+  selected: boolean;
+  disabled: boolean;
 }
 
 export default function Day(props: Props) {
-    let styleId = styles.day;
-    if (props.disabled) {
-        styleId += " " + styles.disabled;
-    } else if (props.highlight) {
-        styleId += " " + styles.highlighted;
-    }
+  let styleId = styles.day;
+  if (props.disabled) {
+    styleId += " " + styles.disabled;
+  } else if (props.highlight) {
+    styleId += " " + styles.highlighted;
+  }
 
-    if (props.selected) {
-        styleId += " " + styles.selected;
-    } 
-    
-    return (
-        <div className={styleId} onClick={() => props.onClick(props.date)}>
-            <p>{props.date}</p>
-            <div className={styles.colorBar}/>
-        </div>
-    );
+  if (props.selected) {
+    styleId += " " + styles.selected;
+  }
+
+  return (
+    <div
+      className={styleId}
+      onClick={() => !props.disabled && props.onClick(props.date)}
+    >
+      <p>{props.date}</p>
+      <div className={styles.colorBar} />
+    </div>
+  );
 }
