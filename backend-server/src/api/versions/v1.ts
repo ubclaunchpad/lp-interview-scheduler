bfc4080d1fb5131aeb7f0dbabef0c27f394d5294
Jin Kim <jink8036@gmail.com>
2022-04-03 21:54:16 -0700

    Add sendgrid integration

--- backend-server/src/api/versions/v1.ts
+++ backend-server/src/api/versions/v1.ts
@@ -3,9 +3,11 @@ import express from "express";
 import { availabilityRouter } from "../routes/availability";
 import { eventRouter } from "../routes/event";
 import { interviewerRouter } from "../routes/interviewer";
+import { emailRouter } from "../routes/emailIntegration";
 
 export const v1Router = express.Router();
 
 v1Router.use("/interviewers", interviewerRouter);
 v1Router.use("/availabilities", availabilityRouter);
 v1Router.use("/events", eventRouter);
+v1Router.use("/email", emailRouter);
